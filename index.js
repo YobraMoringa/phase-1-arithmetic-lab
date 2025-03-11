901d83e92746f7d98e6c3687ae2f97b1843fc830
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-03-11 18:08:07 +0300

    Created variables that store arithmetic operators

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let num3 = 6;
+let num4 = 10;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num4 % num3;
+let max = Math.max(1, 5, 20);
